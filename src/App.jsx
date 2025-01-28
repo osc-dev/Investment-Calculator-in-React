efcb7083afe785fd6a7df20f49ce970f58b3d835
osc-dev <os.chaudhry@hotmail.com>
2025-01-28 23:50:14 +0500

    Results Component - Created a Results component to generate investment calculation results. - Renamed the userInput state to userInputState. - Moved userInputState to the App component so it could be passed onto the Results component. - Moved the handleChange function to the App component to allow state updates.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
+import { useState } from 'react';
+
 import Header from './components/Header.jsx';
 import UserInput from './components/UserInput.jsx';
+import Results from './components/Results.jsx';
 
 function App() {
+  const [userInputState, setUserInputState] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInputState(prevUserInput => {
+        return {
+            ...prevUserInput,
+            [inputIdentifier]: newValue
+        };
+    });
+  }
+
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput onChange={handleChange} userInput={userInputState} />
+      <Results userInput={userInputState} />
     </>
   );
 }
